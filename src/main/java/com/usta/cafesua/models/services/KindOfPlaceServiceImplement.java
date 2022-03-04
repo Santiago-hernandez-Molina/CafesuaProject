2f2860241ebd21eed45dc24eadf9ddf73eb27083
Harrizon Soler <harrizon.soler@usantoto.edu.co>
2022-03-04 10:42:33 -0500

    Fixed @Service on implementations

--- src/main/java/com/usta/cafesua/models/services/KindOfPlaceServiceImplement.java
+++ src/main/java/com/usta/cafesua/models/services/KindOfPlaceServiceImplement.java
@@ -3,11 +3,13 @@ package com.usta.cafesua.models.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.usta.cafesua.entities.KindOfPlace;
 import com.usta.cafesua.models.dao.IKindOfPlaceDao;
 
+@Service
 public class KindOfPlaceServiceImplement implements IKindOfPlace{
 	@Autowired
 	private IKindOfPlaceDao iKindOfPlaceDao;
--- src/main/java/com/usta/cafesua/models/services/PlaceServiceImplement.java
+++ src/main/java/com/usta/cafesua/models/services/PlaceServiceImplement.java
@@ -3,11 +3,13 @@ package com.usta.cafesua.models.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.usta.cafesua.entities.Place;
 import com.usta.cafesua.models.dao.IPlaceDao;
 
+@Service
 public class PlaceServiceImplement implements IPlace {
 	@Autowired
 	private IPlaceDao iPlaceDao;
