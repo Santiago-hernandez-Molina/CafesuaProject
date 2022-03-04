f5d5cbf0fb3c3151f04376add877b7c34b6d2b31
santiagohm <santiago.hernandez@usantoto.edu.co>
2022-03-03 19:55:28 -0500

    bag and status implementation services updated

---
+++ src/main/java/com/usta/cafesua/models/services/BagServiceImplement.java
@@ -0,0 +1,21 @@
+package com.usta.cafesua.models.services;
+
+import com.usta.cafesua.entities.Bag;
+import com.usta.cafesua.models.dao.IBagDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service
+public class BagServiceImplement implements IBag {
+
+    @Autowired
+    private IBagDao iBagDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Bag> findAll(){
+        return (List<Bag>) iBagDao.findAll();
+    }
+}
--- src/main/java/com/usta/cafesua/models/services/IBag.java
+++ src/main/java/com/usta/cafesua/models/services/IBag.java
@@ -2,5 +2,8 @@ package com.usta.cafesua.models.services;
 
 import com.usta.cafesua.entities.Bag;
 
+import java.util.List;
+
 public interface IBag {
+    public List<Bag> findAll();
 }
