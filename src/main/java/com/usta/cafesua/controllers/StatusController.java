0fec0fdc9bec17768be34eb3d117eb6ae88d57d3
Harrizon Soler <harrizon.soler@usantoto.edu.co>
2022-03-14 10:04:19 -0500

    Improved templates

--- src/main/java/com/usta/cafesua/controllers/StatusController.java
+++ src/main/java/com/usta/cafesua/controllers/StatusController.java
@@ -7,13 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.usta.cafesua.entities.Status;
 import com.usta.cafesua.models.services.IStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class StatusController {
@@ -27,30 +24,20 @@ public class StatusController {
         model.addAttribute("status", iStatusService.findAll());
         return "listStatus";
     }
-
+    
     @GetMapping(value = "/createStatus")
-    public String addStatus(Model model) {
-        model.addAttribute("status", new Status());
-        model.addAttribute("title", "Create Status");
+    public String addStatus(Model model){
+        model.addAttribute("status",new Status());
+        model.addAttribute("title","Create Status");
         return "createStatus";
     }
 
-    @PostMapping(value = "/createStatus")
-    public String saveStatus(@Valid Status status, BindingResult result, Model model) {
-        if (result.hasErrors()) {
+    @PostMapping(value="/createStatus")
+    public String saveStatus(@Valid Status status, BindingResult result, Model model){
+        if (result.hasErrors()){
             return "createStatus";
         }
         iStatusService.saveStatus(status);
-        return "redirect:/listStatus";
-    }
-
-    @RequestMapping("/deleteStatus/{id}")
-    public String delete(@PathVariable(value = "id") Long id, RedirectAttributes flash){
-
-        if (id>0){
-            iStatusService.deleteByIdStatus(id);
-            flash.addFlashAttribute("sucess","Delete bag successfully");
-        }
         return "redirect:/listStatus";
     }
 }
